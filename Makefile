9a09fcdd2fab0ae031f97ea5cf73e57e8049a835
cbuilder <alexeev@nateks.ru>
2017-04-07 12:29:37 +0300

    fixed prototype, gcc Wall option

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc capture.c x264_encoder.c -o capture -lx264
+	gcc capture.c x264_encoder.c -o capture -lx264 -Wall
--- x264_encoder.h
+++ x264_encoder.h
@@ -1,4 +1,4 @@
 
 int h264_init(int width, int height);
 void h264_close();
-int h264_encode(int width, int height, unsigned char *FrameIn, unsigned char *FrameOut);
+int h264_encode(int width, int height, unsigned char *FrameIn, unsigned char **FrameOut);
